ffeac1d678ec23e2ff9a7ebbc77f4dc51d84b255
James Hobin <hobinjk@gmail.com>
2018-05-07 15:28:43 -0400

    Add simple install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+npm install
+
+# Create the service file needed by systemctl
+sudo sh -c 'cat > /etc/systemd/system/mozilla-iot-gateway-prometheus-translator.service' <<END
+[Unit]
+Description=Mozilla IoT Gateway to Prometheus Translator
+After=network.target
+
+[Service]
+Type=simple
+StandardOutput=journal
+StandardError=journal
+User=pi
+WorkingDirectory=$(pwd)
+# Edit this line, if needed, to set the correct path to node
+ExecStart=$(pwd)/translator.sh
+Restart=always
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+END
+
+sudo systemctl enable mozilla-iot-gateway-prometheus-translator
+sudo systemctl start mozilla-iot-gateway-prometheus-translator
+
+echo "Done! You can now access your translated metrics at http://gateway.local:3060 or http://localhost:3060"
